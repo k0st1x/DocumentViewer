fb38e31a79d48885baec964e475b80c540055c7b
k0st1x <k0st1x@ya.ru>
2012-07-27 19:57:08 +0400

    initial commit with code

---
+++ WebReportViewer/ReportService1.svc.cs
@@ -0,0 +1,7 @@
+﻿using DevExpress.Data.Utils.ServiceModel;
+
+namespace WebReportViewer {
+    [SilverlightFaultBehavior]
+    public class ReportService1 : DevExpress.XtraReports.Service.ReportService {
+    }
+}
---
+++ WebReportViewer/Scripts/ReportViewer/AjaxReportViewer.js
@@ -0,0 +1,71 @@
+﻿// require jQuery 1.5.1
+
+function WcfClient(url) {
+    this._url = url;
+    this.invoke = function(methodName, data, complete) {
+        var completeInternal = function(data, status) {
+            var response = JSON.parse(data.responseText);
+            complete(response, response.d, status);
+        };
+        var settings = {
+            data: JSON.stringify(data),
+            complete: completeInternal,
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            type: "POST"
+        };
+        $.ajax(this._url + "/" + methodName, settings);
+    };
+}
+
+// DataContract
+function ReportNameIdentity(value) {
+    this.__type = "ReportNameIdentity:http://schemas.datacontract.org/2004/07/DevExpress.Xpf.Printing.ServiceModel.DataContracts";
+    this.Value = value;
+}
+
+var TaskStatus = {
+    Fault: 0,
+    InProgress: 1,
+    Complete: 2
+};
+
+var PageCompatibility = {
+    HTML: 3
+};
+
+function ReportViewerClient(url) {
+    this._client = new WcfClient(url);
+    
+    this.getReportInformation = function(reportName, complete) {
+        var args = { identity: new ReportNameIdentity(reportName) };
+        this._client.invoke("GetReportInformation", args, complete);
+    };
+    
+    this.startBuild = function(reportName, parameters, complete) {
+        var args = {
+            identity: new ReportNameIdentity(reportName),
+            buildArgs: { Parameters: parameters }
+        };
+        this._client.invoke("StartBuild", args, complete);
+    };
+    
+    this.getBuildStatus = function(documentId, complete) {
+        this._client.invoke("GetBuildStatus", { documentId: documentId }, complete);
+    };
+    
+    this.getPage = function(documentId, pageIndex, compatibility, complete) {
+        var args = {
+            documentId: documentId,
+            pageIndex: pageIndex,
+            compatibility: compatibility
+        };
+        this._client.invoke("GetPage", args, complete);
+    };
+}
+
+function CreateReportOperation(reportViewerClient, reportName) {
+    this._client = reportViewerClient;
+    this.start = function() {
+    };
+}
